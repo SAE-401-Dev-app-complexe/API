4436bafb453ad8694e73a287e27d953815a2b5e5
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2024-03-20 22:19:40 +0100

    Recuperation des festivals favoris et non favoris par rapport à la clé Api

--- testAPISAE/API/FestivalService.php
+++ testAPISAE/API/FestivalService.php
@@ -2,9 +2,20 @@
 
 class FestivalService {
 
-    public static function getFestival($pdo): array
+    public static function getFestival($pdo,$cle): array
     {
-        $stmt = $pdo->prepare("SELECT * FROM festival WHERE dateDebut > NOW() ORDER BY dateDebut ASC");
+        
+        $stmt = $pdo->prepare("SELECT festival.idFestival, festival.titre, festival.illustration, festival.dateDebut, festival.dateFin, 
+        CASE 
+        WHEN utilisateur.idUtilisateur IS NOT NULL THEN true
+        ELSE false
+        END as favoris
+        FROM festival
+        LEFT JOIN festivalsfavoris ON festival.idFestival = festivalsfavoris.idFestival
+        LEFT JOIN utilisateur ON festivalsfavoris.idUtilisateur = utilisateur.idUtilisateur
+        WHERE festival.dateFin > NOW() 
+        AND (utilisateur.cleApi = :cleApi OR festivalsfavoris.idFestival IS NULL)");
+        $stmt->bindParam("cleApi",$cle);
         $stmt->execute();
         return $stmt->fetchAll();
     }
