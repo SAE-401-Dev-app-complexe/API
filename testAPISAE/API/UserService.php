f791ddc3b819ec9fdb4c4616e9f58fd34c97f54a
Simon Guiraud <78082685+SyberSim@users.noreply.github.com>
2024-03-20 11:22:47 +0100

    Ajout méthode inscription et méthode pour get un utilisateur (renvoi du nom et prénom)

--- testAPISAE/API/UserService.php
+++ testAPISAE/API/UserService.php
@@ -9,4 +9,24 @@ class UserService {
         $stmt->execute();
         return count($stmt->fetchAll()) > 0 ? array("cleApi" => "testApiKey") : array("cleApi" => null);
     }
+
+    public static function inscription($prenom, $nom, $mail, $login, $mdp, $pdo): array
+    {
+        $stmt = $pdo->prepare("INSERT INTO utilisateur (prenom , nom, mail,login, mdp ) VALUES (:prenom, :nom, :mail, :login, :mdp)");
+        $stmt->bindParam("login", $login);
+        $stmt->bindParam("mdp", $mdp);
+        $stmt->bindParam("mail", $mail);
+        $stmt->bindParam("nom", $nom);
+        $stmt->bindParam("prenom", $prenom);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public static function getUser($cleApi, $pdo): array
+    {
+        $stmt = $pdo->prepare("SELECT nom, prenom FROM utilisateur WHERE cleApi = :cleApi");
+        $stmt-> bindParam("cleApi" , $cleApi);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
