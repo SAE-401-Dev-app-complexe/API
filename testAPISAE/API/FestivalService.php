7e95cad383613b4b4403c9e06b5701995be05c93
Simon Guiraud <78082685+SyberSim@users.noreply.github.com>
2024-03-22 09:40:03 +0100

    Format + order by des dates pour l'affichage

--- testAPISAE/API/FestivalService.php
+++ testAPISAE/API/FestivalService.php
@@ -6,7 +6,7 @@ class FestivalService {
     {
         
         $stmt = $pdo->prepare("
-        SELECT festival.idFestival, festival.titre,festival.categorie, festival.illustration, festival.description, festival.dateDebut, festival.dateFin, 
+        SELECT festival.idFestival, festival.titre,festival.categorie, festival.illustration, festival.description, DATE_FORMAT(festival.dateDebut, '%d/%m/%Y'), DATE_FORMAT(festival.dateFin, '%d/%m/%Y'), 
         CASE 
         WHEN utilisateur.idUtilisateur IS NOT NULL THEN true
         ELSE false
@@ -15,7 +15,8 @@ class FestivalService {
         LEFT JOIN festivalsfavoris ON festival.idFestival = festivalsfavoris.idFestival
         LEFT JOIN utilisateur ON festivalsfavoris.idUtilisateur = utilisateur.idUtilisateur
         WHERE festival.dateDebut >= NOW() 
-        AND (utilisateur.cleApi = :cleApi OR festivalsfavoris.idFestival IS NULL)");
+        AND (utilisateur.cleApi = :cleApi OR festivalsfavoris.idFestival IS NULL)
+        ORDER BY festival.dateDebut ASC");
         $stmt->bindParam("cleApi",$cle);
         $stmt->execute();
         return $stmt->fetchAll();
